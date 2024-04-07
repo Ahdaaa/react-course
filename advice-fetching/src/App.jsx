6cafba6e0e30d5dda878666f72b51e4cc935185a
Ahdaaa <afilzag@gmail.com>
2024-04-07 13:32:50 +0700

    feat: fetch after opening page

--- advice-fetching/src/App.jsx
+++ advice-fetching/src/App.jsx
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [advice, setAdvice] = useState("")
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0); // initial count 0
  
 
   async function getAdvice() {
     const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
   }
+
+  useEffect(function() { // init fetching after opening page
+    getAdvice();
+  }, [])
 
   return (
     <div>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
     </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>You have read <strong>{props.count}</strong> pieces of advice</p>
   );
 }
 
