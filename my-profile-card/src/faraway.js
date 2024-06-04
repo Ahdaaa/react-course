15491acf4f2d1eb9c62b95e4970cdc876fe161bd
Ahdaaa <afilzag@gmail.com>
2024-06-04 23:39:55 +0700

    feat: implement sort items by

--- my-profile-card/src/faraway.js
+++ my-profile-card/src/faraway.js
@@ -99,21 +99,54 @@ function Form({ onAddItems }) {
 }
 
 function Packing({ itemsObj, onDeleteItem, onCheckItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = itemsObj;
+
+  if (sortBy === "description") {
+    sortedItems = itemsObj
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = itemsObj
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
-    <ul className="font-ptSans font-semibold mb-5 lg:w-3/6 text-center grid grid-cols-3 gap-5">
-      {itemsObj.map((item) => (
-        <div className="flex flex-row gap-2">
-          <input
-            type="checkbox"
-            id="default-checkbox"
-            value={item.packed}
-            className="checkbox"
-            onChange={() => onCheckItem(item.id)}
-          />
-          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
-        </div>
-      ))}
-    </ul>
+    <>
+      <ul className="font-ptSans font-semibold mb-5 lg:w-3/6 text-center grid grid-cols-3 gap-5">
+        {sortedItems.map((item) => (
+          <div className="flex flex-row gap-2" key={item.id}>
+            <input
+              type="checkbox"
+              id="default-checkbox"
+              className="checkbox"
+              checked={item.packed}
+              value=""
+              onChange={() => onCheckItem(item.id)}
+            />
+            <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
+          </div>
+        ))}
+      </ul>
+
+      <div>
+        <select
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+          className="font-ptSans text-black bg-white hover:bg-gray-300 rounded-full text-sm px-5 py-2 me-2 mb-2 "
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
+    </>
   );
 }
 
@@ -136,7 +169,8 @@ function Stats({ itemsObj }) {
     <footer>
       <em className="font-ptSans lg:text-xl text-sm">
         💼 You have {numItems} items on your list, and you already packed{" "}
-        {numPacked} ({numItems > 0 ? `${(numPacked / numItems) * 100}%` : `0%`})
+        {numPacked} (
+        {numItems > 0 ? `${Math.floor((numPacked / numItems) * 100)}%` : `0%`})
       </em>
     </footer>
   );
